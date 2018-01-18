649a6578358a2f97283f787e3711455e86d073ca
itsHabib <michaelhabib1868@gmail.com>
2018-01-17 23:29:05 -0800

    test ProcessRequest from client and API call from api

--- api_test.go
+++ api_test.go
@@ -1,1 +1,18 @@
 package nap
+
+import (
+	"testing"
+	"net/http"
+)
+func TestAPICall(t *testing.T) {
+	api := NewAPI("https://httpbin.org")
+	router := NewRouter()
+	router.RegisterFunc(200, func(resp *http.Response, _ interface{}) error {
+		return nil
+	})
+	res := NewResource("/get", "GET", router)
+	api.AddResource("get", res)
+	if err := api.Call("get", nil); err != nil {
+		t.Fail()
+	}
+}
